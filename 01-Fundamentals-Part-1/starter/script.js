de5a7da233268f956a2162c9295916cfe80ff6ae
CarmenWalliser <carmenwalliser2004@gmail.com>
2025-09-09 12:47:58 +0200

    data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,30 @@
-let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!"); // This is where your JavaScript code will go
+// let js = "amazing";
+// if (js === "amazing") alert("JavaScript is FUN!"); // This is where your JavaScript code will go
 
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+// 40 + 8 + 23 - 10;
+// console.log(40 + 8 + 23 - 10);
+
+// //Values and variables
+// let country = "South Africa";
+// let continebt = "Gauteng";
+// let population = 650000000;
+// console.log(country);
+// console.log(continebt);
+// console.log(population);
+
+// //Data Types
+// let isIsland = false;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+//let, const and var
+language = "Afrikaans";
+const country = "South Africa";
+const continent = "Gauteng";
+const isLand = false;
+isIsland = true;
+console.log(isIsland);
