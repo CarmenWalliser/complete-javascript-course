4a2a716992718863825b0374601679975414b841
CarmenWalliser <carmenwalliser2004@gmail.com>
2025-09-16 10:56:41 +0200

    start with part 2

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive !");
+
+//die woorde bestaan klaar vir iets anders
+// const interface = "Audio";
+// const private = 534;
+
+//functions
+// function logger() {
+//   console.log("My name is Jonas");
+// }
+// //calling, running, invoking a function
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges `;
+//   return juice;
+// }
+
+// const appleOranjeJuice = fruitProcessor(5, 0);
+// console.log(appleOranjeJuice);
+
+// //function declaration
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
+// const age1 = calcAge1(1991);
+// console.log(age1);
+
+// //function expression
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+// const age2 = calcAge2(1991);
+// console.log(age2);
