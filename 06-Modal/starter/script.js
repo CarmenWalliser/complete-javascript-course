244ae25cbf02c83f4b2d5519b076466339c2d9ab
CarmenWalliser <carmenwalliser2004@gmail.com>
2025-10-04 09:03:35 +0200

    Modal window

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const model = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+console.log(btnShowModal);
+
+const openModal = function () {
+  //console.log('Button clicked');
+  model.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  model.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', openModal);
+  //   btnShowModal[i].addEventListener('click', function () {
+  //     console.log('Button clicked');
+  //     model.classList.remove('hidden');
+  //     overlay.classList.remove('hidden');
+  //   });
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  console.log(event.key);
+
+  if (event.key === 'Escape' && !model.classList.contains('hidden')) {
+    closeModal();
+  }
+  // model.classList.add('hidden');
+  // overlay.classList.add('hidden');
+});
