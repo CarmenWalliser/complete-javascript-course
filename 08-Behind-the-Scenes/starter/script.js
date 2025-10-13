334c315306865df325cdcefa55e207f5edd17757
CarmenWalliser <carmenwalliser2004@gmail.com>
2025-10-13 11:52:52 +0200

    objects

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,26 +1,130 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    const output = `${firstName}, are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     const output = `${firstName}, are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      const str = `Oh, and youre a millenial, ${firstName}`;
-      console.log(str);
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       const str = `Oh, and youre a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
-  }
-  printAge();
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//   }
+//   printAge();
 
-  return age;
+//   return age;
+// }
+
+// const firstName = 'Jonas';
+// calcAge(1991);
+// //printAge();
+
+//--------------------------------------------------
+// //hoisting with variables
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
+
+// //Functions
+// console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+
+//Hoisting Example
+
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log('All products deleted');
+// }
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+//--------------------------------------------------
+//this keyword
+//console.log(this);
+
+// const calcAge = function (birthYear) {
+//   console.log(20237 - birthYear);
+//   //console.log(this);
+// };
+// calcAge(1991);
+
+// const jonas = {
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   },
+// };
+// jonas.calcAge();
+
+// const matilda = {
+//   year: 2017,
+// };
+
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// f();
+
+//object
+
+const jessica1 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+function marryPerson(originperson, newLastName) {
+  originperson.lastName = newLastName;
+  return originperson;
 }
+const marriadJessia = marryPerson(jessica1, 'Davis');
 
-const firstName = 'Jonas';
-calcAge(1991);
-//printAge();
+// const marriadJessia = jessica;
+// marriadJessia.lastName = 'Davis';
+
+// console.log('Before:', jessica1);
+// console.log('after', marriadJessia);
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = { ...jessica };
+jessicaCopy.lastName = 'Davis';
+
+console.log(jessica, jessicaCopy);
+jessicaCopy.family.push('Mary');
+
+console.log('Before:', jessica);
+console.log('after', jessicaCopy);
